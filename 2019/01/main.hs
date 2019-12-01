49e45f63a316d425a30352ee23ef4eea11913dcc
wdhg <wdhgrant@gmail.com>
2019-12-01 14:58:58 +0000

    add day 01

---
+++ 2019/01/main.hs
@@ -0,0 +1,29 @@
+import System.IO
+
+getMasses :: IO [Int]
+getMasses
+  = do
+    file <- readFile "input"
+    return $ map read (lines file) :: IO [Int]
+
+mapAndSum :: (Int -> Int) -> IO Int
+mapAndSum f
+  = do
+    masses <- getMasses
+    return $ sum $ map f masses
+
+calculateFuel :: Int -> Int
+calculateFuel mass
+  = mass `div` 3 - 2
+
+part1 :: IO Int
+part1
+  = mapAndSum calculateFuel
+
+part2 :: IO Int
+part2
+  = mapAndSum calculateTotalFuel
+      where
+        calculateTotalFuel :: Int -> Int
+        calculateTotalFuel mass
+          = sum $ tail $ takeWhile (>0) $ iterate calculateFuel mass
